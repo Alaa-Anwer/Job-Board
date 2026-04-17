1a5031be392149cd22d12fad6fadabd402322f35
selim <ahmedadelll364@gmail.com>
2026-04-17 10:00:00 +0200

    add the catigory model

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    // Returns jobs linked to this category.
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class, 'job_category');
+    }
+}
